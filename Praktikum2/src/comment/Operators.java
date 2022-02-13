437c1949424a2c4d7f567effd0508323e4b3727f
dafanf <dafanurulfauziansyah97@gmail.com>
2022-02-13 19:34:40 +0700

    adding operator case

---
+++ Praktikum2/src/comment/Operators.java
@@ -0,0 +1,13 @@
+package comment;
+
+/**
+ * @author  DAFA N.F
+ * @version 1.0
+ * @since   2022-02-13
+ */
+public class Operators {
+    public static void main(String[] args){
+        double x = 92.88;
+        int nx = (int) Math.round(x);
+    }
+}
