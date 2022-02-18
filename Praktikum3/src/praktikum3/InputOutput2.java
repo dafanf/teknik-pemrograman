1ead08a7b2cdd7aa61b962a9cf72106670ef4e26
dafanf <dafanurulfauziansyah97@gmail.com>
2022-02-18 21:50:42 +0700

    updating code

--- Praktikum3/src/praktikum3/InputOutput2.java
+++ Praktikum3/src/praktikum3/InputOutput2.java
@@ -2,7 +2,7 @@ package praktikum3;
 import java.util.Scanner;
 /**
  * @author  DAFA N.F
- * @version 1.0
+ * @version 1.1
  * @since   2022-02-17
  */
 public class InputOutput2 {
@@ -19,8 +19,6 @@ public class InputOutput2 {
         testValue = new int[totalCase];
         for(counter = 0; counter < totalCase; counter++){
             nowCase = counter + 1;
-            System.out.println("Case - "+nowCase);
-            System.out.print("Please Input a word followed by number divided by whitespace ex (python 100) : ");
             testString[counter] = in.next();
             testValue[counter] = in.nextInt();
         }
