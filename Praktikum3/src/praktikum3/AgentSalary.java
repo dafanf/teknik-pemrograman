4a19dee33351b9f5bbdffd97409ae0ad3ea3b629
dafanf <dafanurulfauziansyah97@gmail.com>
2022-02-18 22:22:11 +0700

    adding agent salary case

---
+++ Praktikum3/src/praktikum3/AgentSalary.java
@@ -0,0 +1,32 @@
+package praktikum3;
+
+import java.util.Scanner;
+
+/**
+ * @author  DAFA N.F
+ * @version 1.0
+ * @since   2022-02-18
+ */
+public class AgentSalary {
+    public static void main(String[] args){
+        int basicSalary = 500000, totalSalary, itemPrice = 50000, itemSold;
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Please enter the number of items sold this month : ");
+        itemSold = in.nextInt();
+        
+        if(itemSold < 15){
+            totalSalary = basicSalary - (15-itemSold) * itemPrice * 15/100;
+        }
+        else if(itemSold >= 40 && itemSold <= 80){
+            totalSalary = basicSalary + (itemSold*itemPrice*25/100);
+        }
+        else if(itemSold > 80){
+            totalSalary = basicSalary + (itemSold*itemPrice*35/100);
+        }
+        else{
+            totalSalary = basicSalary + (itemSold*itemPrice*10/100);
+        }
+        System.out.println("Total agent salary = "+totalSalary);
+    }
+}
