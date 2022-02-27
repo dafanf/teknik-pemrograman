71051109b629ba41526d9d1e55461c61e0a2bc33
dafanf <dafanurulfauziansyah97@gmail.com>
2022-02-27 11:41:23 +0700

    adding case study 3

---
+++ Praktikum4/src/praktikum4/Kelas1.java
@@ -0,0 +1,22 @@
+package praktikum4;
+
+/**
+ *
+ * @author DAFA N.F
+ */
+public class Kelas1{
+    {
+        System.out.println(11);
+    }
+    static{
+        System.out.println(2);
+    }
+    public Kelas1(int i)
+    {
+        System.out.println(3);
+    }
+    public Kelas1()
+    {
+        System.out.println(4);
+    }
+}
---
+++ Praktikum4/src/praktikum4/Kelas2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum4;
+
+/**
+ *
+ * @author DAFA N.F
+ */
+public class Kelas2{
+    {
+        System.out.println(5);
+    }
+    public static void main(String[] args){
+        System.out.println(6);
+        Kelas1 satu = new Kelas1();
+        Kelas1 dua = new Kelas1(10);
+    }
+}
